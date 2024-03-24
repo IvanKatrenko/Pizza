3248e4e2a84a9c6a2498e6ae4b271fa72d002092
berno_4444 <ivankatrenko1997@gmail.com>
2024-03-24 14:15:19 +0100

    new file

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const toppingsClose = document.querySelector('.toppings__close');
+const toppingsList = document.querySelector('.toppings__list');
+
+toppingsClose.addEventListener('click', () => {
+    if (!toppingsList.classList.contains('toppings__list_show')) {
+        toppingsList.classList.add('toppings__list_show');
+        toppingsClose.classList.add('toppings__close_active');
+
+        toppingsList.style.maxHeight = toppingsList.scrollHeight + 'px';
+    } else {
+        toppingsList.classList.remove('toppings__list_show');
+        toppingsClose.classList.remove('toppings__close_show');
+        toppingsClose.classList.remove('toppings__close_active');
+        toppingsList.style.maxHeight = null;
+
+        setTimeout(() => {
+            toppingsList.style.remove('toppings__list_show');
+        }, 300)
+    }
+})
